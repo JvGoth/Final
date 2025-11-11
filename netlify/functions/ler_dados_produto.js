700cbdde044c2161ebe11075129703d4061edc7b
JvGoth <joaovitorelias000@gmail.com>
2025-11-11 17:37:43 -0300

    Update ler_dados_produto.js

--- netlify/functions/ler_dados_produto.js
+++ netlify/functions/ler_dados_produto.js
@@ -3,7 +3,6 @@
 const { getStore } = require("@netlify/blobs");
 
 exports.handler = async (event) => {
-  // O ID do produto é passado como parâmetro na URL: ?id=123456
   const idChave = event.queryStringParameters.id; 
 
   if (!idChave) {
@@ -16,20 +15,15 @@ exports.handler = async (event) => {
   try {
     const store = getStore("produtos_bling");
     
-    // 1. Busca os dados e armazena em 'produtoDados'
     const produtoDados = await store.getJSON(idChave); 
 
-    // 2. Verifica se 'produtoDados' existe
     if (produtoDados) { 
-      // Retorna os dados do produto para o front-end
       return {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        // 3. Serializa 'produtoDados'
         body: JSON.stringify(produtoDados), 
       };
     } else {
-      // Produto não encontrado na base (Netlify Blobs)
       return {
         statusCode: 404,
         body: JSON.stringify({ error: "Produto não encontrado ou ainda não sincronizado." }),
