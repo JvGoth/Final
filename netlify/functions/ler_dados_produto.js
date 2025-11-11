88e92f4a98a42beb0c897120046b2ca367b3ddc3
JvGoth <joaovitorelias000@gmail.com>
2025-11-11 20:54:56 -0300

    Update ler_dados_produto.js

--- netlify/functions/ler_dados_produto.js
+++ netlify/functions/ler_dados_produto.js
@@ -13,11 +13,7 @@ exports.handler = async (event) => {
   }
 
   try {
-    const store = getStore({
-      name: "produtos_bling",
-      siteID: process.env.NETLIFY_SITE_ID,
-      token: process.env.NETLIFY_API_TOKEN
-    });
+    const store = getStore("produtos_bling");
     
     const produtoDados = await store.get(idChave, { type: "json" }); 
 
