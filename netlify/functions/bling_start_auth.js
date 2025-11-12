9d10c7ab778ab77dfdbaf916340b6fbe76fcb27b
JvGoth <joaovitorelias000@gmail.com>
2025-11-11 21:53:02 -0300

    Update bling_start_auth.js

--- netlify/functions/bling_start_auth.js
+++ netlify/functions/bling_start_auth.js
@@ -7,9 +7,9 @@ exports.handler = async () => {
     try {
         const state = crypto.randomBytes(16).toString('hex');
         const store = getStore({
-            name: "bling_tokens",
+            name: "seu_name",
             siteID: process.env.NETLIFY_SITE_ID,
-            token: process.env.NETLIFY_API_TOKEN
+            token: process.env.NETLIFY_AUTH_TOKEN
         });
         await store.set("state", state);
 
